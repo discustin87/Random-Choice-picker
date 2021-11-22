65601429191c317c7ee1dc50b52f397b76eb31ac
Justin Croteau <justinwebdev1@gmail.com>
2021-11-22 17:43:34 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const tagsEl = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
+
+textarea.focus();
+
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value);
+
+         if (e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10);
+
+        randomSelect()
+    }
+
+});
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerHTML = tag
+        tagsEl.appendChild(tagEl)
+    });
+}
+
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highlighthTag(randomTag)
+
+        setTimeout(() => {
+            unHighlighthTag(randomTag)
+        }, 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlighthTag(randomTag)
+        }, 100);
+    }, times * 100);
+}
+console.log(textarea.value);
+
+
+ function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlighthTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unHighlighthTag(tag) {
+    tag.classList.remove('highlight')
+}
